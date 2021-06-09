5ae8cc7970de0c75cc98a1d4e6f859901df9f402
42zeroo <42zeroo@gmail.com>
2021-06-09 11:16:10 +0200

    Change wojewodztwa letter case

--- src/functions/inWojewodztwoTranslator.js
+++ src/functions/inWojewodztwoTranslator.js
@@ -1,37 +1,37 @@
 const inWojewodztwoTranslator = (wojName) => {
   switch (wojName) {
     case "Lubelskie":
-      return "w wojewodztwie Lubelskim";
+      return "w województwie lubelskim";
     case "Podlaskie":
-      return "w wojewodztwie Podlaskim";
+      return "w województwie podlaskim";
     case "Mazowieckie":
-      return "w wojewodztwie Mazowieckim";
+      return "w województwie mazowieckim";
     case "Podkarpackie":
-      return "w wojewodztwie Podkarpackim";
+      return "w województwie podkarpackim";
     case "Malopolskie":
-      return "w wojewodztwie Małopolskim";
+      return "w województwie małopolskim";
     case "Swietokrzyskie":
-      return "w wojewodztwie Świętokrzyskim";
+      return "w województwie świętokrzyskim";
     case "Warminsko-Mazurskie":
-      return "w wojewodztwie Warmińsko-Mazurskim";
+      return "w województwie warmińsko-mazurskim";
     case "Kujawsko-Pomorskie":
-      return "w wojewodztwie Kujawsko-Pomorskim";
+      return "w województwie kujawsko-pomorskim";
     case "Pomorskie":
-      return "w wojewodztwie Pomorskim";
+      return "w województwie pomorskim";
     case "Zachodniopomorskie":
-      return "w wojewodztwie Zachodniopomorskim";
+      return "w województwie zachodniopomorskim";
     case "Lubuskie":
-      return "w wojewodztwie Lubuskim";
+      return "w województwie lubuskim";
     case "Dolnoslaskie":
-      return "w wojewodztwie Dolnośląskim";
+      return "w województwie dolnośląskim";
     case "Lodzkie":
-      return "w wojewodztwie Łódzkim";
+      return "w województwie łódzkim";
     case "Wielkopolskie":
-      return "w wojewodztwie Wielkopolskim";
+      return "w województwie wielkopolskim";
     case "Slaskie":
-      return "w wojewodztwie Śląskim";
+      return "w województwie śląskim";
     case "Opolskie":
-      return "w wojewodztwie Opolskim";
+      return "w województwie opolskim";
     case "Cala Polska":
       return "na terenie całego kraju";
     case "Polska Wschodnia":
