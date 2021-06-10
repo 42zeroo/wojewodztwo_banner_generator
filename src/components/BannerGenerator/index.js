1ac65c0a9595a1b03930ebf978e91f4deac70466
42zeroo <42zeroo@gmail.com>
2021-06-10 17:18:03 +0200

    xx

--- src/components/BannerGenerator/index.js
+++ src/components/BannerGenerator/index.js
@@ -10,11 +10,6 @@ const BannerGenerator = ({ idToRender }) => {
   const [wojewodztwoToFill, setWojewodztwoToFill] = useState("");
   return (
     <>
-      <h1>
-        JAK CHCESZ 5 TO MOGE CI ZA 200 ODDAC KOZAKA, ALE TANIEJ NA PEWNO NIE BO
-        KONCOWE MAM.
-      </h1>
-      <h2>Jak chcesz wieksza paczke taniej to jutro</h2>
       <Form
         idToRender={idToRender}
         setImage={setImage}
